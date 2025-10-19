d7566455ca4a6b8acca1c8bc0a998d59da757a71
khachuynguyen0 <khachuynguyen0@gmail.com>
2025-10-19 10:33:50 +0700

    up pj win-form

---
+++ test-code-p1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_code_p1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
